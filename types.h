be18791d12829996e49c19a88e3789661e9dd30d
DisclaimedGoat <kyleturleywas@gmail.com>
2024-11-25 09:39:30 -0700

    fixed uint48 errors

--- types.h
+++ types.h
@@ -1,9 +1,11 @@
+#pragma once
+
 #define SIZEX 16
 #define SIZEY 24
 
 typedef unsigned short array[24];
 
-typedef struct {
+typedef struct uint48 {
 	unsigned short top16;
 	unsigned short mid16;
 	unsigned short bot16;
